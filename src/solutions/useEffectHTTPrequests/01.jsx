d0ed8f2cc652649ff7066f909043bbeadb756381
luke afolabi <admin@asynclearning.io>
2021-03-11 20:49:43 -0800

    useEffect-HTTPRequest Drill down

--- src/solutions/useEffectHTTPrequests/01.jsx
+++ src/solutions/useEffectHTTPrequests/01.jsx
@@ -1,63 +1,80 @@
 // useEffect: HTTP requests
-import * as React from 'react'
+import * as React from "react";
 // 🐨 you'll want the following additional things from '../pokemon':
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import { PokemonForm, PokemonInfoFallback, PokemonDataView, fetchPokemon } from '../../utils/pokemon'
+import {
+  PokemonForm,
+  PokemonInfoFallback,
+  PokemonDataView,
+  fetchPokemon,
+} from "../../utils/pokemon";
 
-import { WorkShopNote } from '../../reusables/workshop-note'
-import file from '../../exercise/useEffectHTTPrequests/01.md'
+import { WorkShopNote } from "../../reusables/workshop-note";
+import file from "../../exercise/useEffectHTTPrequests/01.md";
 
+function PokemonInfo({ pokemonName }) {
+  const [pokemon, setPokemon] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
-function PokemonInfo({pokemonName}) {
-    const [pokemon, setPokemon] = React.useState(null);
- 
-    React.useEffect(() => {
-        if (!pokemonName) {
-            return
-        }
-        setPokemon(null)
-        fetchPokemon(pokemonName).then( pokemonData => setPokemon(pokemonData))
-    }, [pokemonName])
-
-  
-    if(!pokemonName) {
-        return 'Submit a pokemon'
-    } else if(!pokemon) {
-        return <PokemonInfoFallback name={pokemonName} />
-    } else {
-        return <PokemonDataView pokemon={pokemon} />
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return;
     }
+
+    setPokemon(null);
+    setError(null);
+
+    fetchPokemon(pokemonName).then(
+      (pokemonData) => setPokemon(pokemonData),
+      (error) => setError(error)
+    );
+  }, [pokemonName]);
+
+  if (error) {
+    return (
+      <div role="alert">
+        There was an error:{" "}
+        <pre style={{ whiteSpace: "normal" }}>{error.message}</pre>
+      </div>
+    );
   }
+
+  if (!pokemonName) {
+    return "Submit a pokemon";
+  } else if (!pokemon) {
+    return <PokemonInfoFallback name={pokemonName} />;
+  } else {
+    return <PokemonDataView pokemon={pokemon} />;
+  }
+}
 
 function PokemonApp() {
-    const [pokemonName, setPokemonName] = React.useState('')
-  
-    function handleSubmit(newPokemonName) {
-      setPokemonName(newPokemonName)
-    }
-  
-    return (
-      <div className="pokemon-info-app">
-        <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
-        <hr />
-        <div className="pokemon-info">
-          <PokemonInfo pokemonName={pokemonName} />
-        </div>
-      </div>
-    )
+  const [pokemonName, setPokemonName] = React.useState("");
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName);
   }
 
-
-  function App() {
-    return (
-      <div className="grid-container">
-          <WorkShopNote file={file} />
-          <PokemonApp />
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonInfo pokemonName={pokemonName} />
       </div>
-     
-    )
-  }
-  
-  export default App
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="grid-container">
+      <WorkShopNote file={file} />
+      <PokemonApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/solutions/useEffectHTTPrequests/01_extra-3.jsx
@@ -0,0 +1,96 @@
+// useEffect: HTTP requests
+import * as React from "react";
+// 🐨 you'll want the following additional things from '../pokemon':
+// fetchPokemon: the function we call to get the pokemon info
+// PokemonInfoFallback: the thing we show while we're loading the pokemon info
+// PokemonDataView: the stuff we use to display the pokemon info
+import {
+  PokemonForm,
+  PokemonInfoFallback,
+  PokemonDataView,
+  fetchPokemon,
+} from "../../utils/pokemon";
+
+import { WorkShopNote } from "../../reusables/workshop-note";
+import file from "../../exercise/useEffectHTTPrequests/01.md";
+
+function PokemonInfo({ pokemonName }) {
+  //   const [pokemon, setPokemon] = React.useState(null);
+  //   const [error, setError] = React.useState(null);
+  //   const [status, setStatus] = React.useState("idle");
+
+  // const [ state, setState] = React.useState({ status: 'idle', pokemon: null, error: null });
+  // state = { status: 'idle', pokemon: null, error: null }
+
+  //object destructuring
+
+  const [state, setState] = React.useState({
+    status: "idle",
+    pokemon: null,
+    error: null,
+  });
+  console.log(state);
+
+  const { status, pokemon, error } = state;
+
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return;
+    }
+    setState({ status: "pending" });
+    fetchPokemon(pokemonName).then(
+      (pokemon) => {
+        setState({ pokemon, status: "resolved" });
+      },
+      (error) => {
+        setState({ error, status: "rejected" });
+      }
+    );
+  }, [pokemonName]);
+
+  if (status === "idle") {
+    return "Submit a pokemon";
+  } else if (status === "pending") {
+    return <PokemonInfoFallback name={pokemonName} />;
+  } else if (status === "rejected") {
+    return (
+      <div role="alert">
+        There was an error:{" "}
+        <pre style={{ whiteSpace: "normal" }}>{error.message}</pre>
+      </div>
+    );
+  } else if (status === "resolved") {
+    return <PokemonDataView pokemon={pokemon} />;
+  }
+
+  throw new Error("It is impossilbe to get here!");
+}
+
+function PokemonApp() {
+  const [pokemonName, setPokemonName] = React.useState("");
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName);
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonInfo pokemonName={pokemonName} />
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="grid-container">
+      <WorkShopNote file={file} />
+      <PokemonApp />
+    </div>
+  );
+}
+
+export default App;
