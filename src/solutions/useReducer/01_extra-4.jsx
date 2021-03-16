3f36361ab655da44b59a486937e4e57d4d8c72c3
luke afolabi <admin@asynclearning.io>
2021-03-16 15:32:40 -0700

    useReducer - traditional dispatch

--- src/solutions/useReducer/01_extra-4.jsx
+++ src/solutions/useReducer/01_extra-4.jsx
@@ -6,18 +6,26 @@ import * as React from "react";
 import { WorkShopNote } from "../../reusables/workshop-note";
 import file from "../../exercise/useReducer/01.md";
 
-const countReducer = (state, action) => ({
-  ...state,
-  ...(typeof action === "function" ? action(state) : action),
-});
+const countReducer = (state, action) => {
+  const { type, step } = action;
+  switch (type) {
+    case "INCREMENT": {
+      return {
+        ...state,
+        count: state.count + step,
+      };
+    }
+    default:
+      throw new Error(`Unsupported action type" ${action.type}`);
+  }
+};
 
 function Counter({ initialCount = 0, step = 1 }) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   });
   const { count } = state;
-  const increment = () =>
-    setState((currentState) => ({ count: currentState.count + step }));
+  const increment = () => dispatch({ type: "INCREMENT" });
   return <button onClick={increment}>{count}</button>;
 }
 
