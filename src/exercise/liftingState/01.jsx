89a61f70f0e7ea697395fac5b3675148ef4d31e7
luke afolabi <admin@asynclearning.io>
2021-03-15 17:58:11 -0700

    useReducer hook implementation

--- src/exercise/liftingState/01.jsx
+++ src/exercise/liftingState/01.jsx
@@ -1,26 +1,29 @@
 // Lifting state
 
-import React from 'react'
+import React from "react";
 
-import { WorkShopNote } from '../../reusables/workshop-note'
-import file from './01.md'
+import { WorkShopNote } from "../../reusables/workshop-note";
+import file from "./01.md";
 
 /**
  * Write functionality to display user input
  */
-function UserInputDisplay(){
-    // 💣 remove this code 
-    return <div><p>Put your code here</p></div>
-}
-
-function App(){
-    return(
-        <div className="grid-container">
-        <WorkShopNote file={file} />
-        <UserInputDisplay />
+function UserInputDisplay() {
+  // 💣 remove this code
+  return (
+    <div>
+      <p>Put your code here</p>
     </div>
-    ) 
-    
+  );
 }
 
-export default App
+function App() {
+  return (
+    <div className="grid-container">
+      <WorkShopNote file={file} />
+      <UserInputDisplay />
+    </div>
+  );
+}
+
+export default App;
