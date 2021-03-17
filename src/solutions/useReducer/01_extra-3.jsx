6b17b9554a193b23883ef79cbbd4cc743040beb5
moyoakin <moyoakin1211@gmail.com>
2021-03-16 20:32:42 -0700

    useReducerDrillDownDown2

--- src/solutions/useReducer/01_extra-3.jsx
+++ src/solutions/useReducer/01_extra-3.jsx
@@ -1,23 +1,20 @@
 // useReducer: simple Counter
-// 💯 simulate setState with an object OR function
 
 import * as React from "react";
 
 import { WorkShopNote } from "../../reusables/workshop-note";
 import file from "../../exercise/useReducer/01.md";
 
-const countReducer = (state, action) => ({
-  ...state,
-  ...(typeof action === "function" ? action(state) : action),
-});
+const countReducer = (count, change) => count + change;
 
 function Counter({ initialCount = 0, step = 1 }) {
   const [state, setState] = React.useReducer(countReducer, {
-    count: initialCount,
+    count:initialCount
   });
-  const { count } = state;
-  const increment = () =>
-    setState((currentState) => ({ count: currentState.count + step }));
+
+  const {count} = state
+
+  const increment = () => setState({count: count + step});
   return <button onClick={increment}>{count}</button>;
 }
 
